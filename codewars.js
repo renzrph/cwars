7a48e6ac0b2bbd1a2a497608dfdfbad194ae9d54
renzrph <renzestorrph@gmail.com>
2024-03-19 18:57:03 +0800

    Updated script file.

--- codewars.js
+++ codewars.js
@@ -109,3 +109,75 @@ const countPhotos = (road) => {
 
   return photoCount;
 };
+
+/* ------------------- Compare Versions ------------------- */
+function isFloat(number) {
+  return !Number.isInteger(number);
+}
+
+function compareVersions(version1, version2) {
+  let v1 = version1.split(",").map((str) => Number(str));
+  let v2 = version2.split(",").map((str) => Number(str));
+  let versions = v1.concat(v2);
+  console.log(versions);
+
+  if (versions[0] >= versions[1]) {
+    return true;
+  }
+
+  if (isNaN(versions[0]) || isNaN(versions[1])) {
+    return false;
+  }
+
+  if (isFloat(versions[0]) || isFloat(versions[1])) {
+    return false;
+  }
+
+  if (versions[0] === versions[1]) {
+    let subversions1 = version1.split(",").length;
+    let subversions2 = version2.split(",").length;
+
+    if (subversions1 > subversions2) {
+      return true;
+    } else if (subversions1 < subversions2) {
+      return false;
+    }
+  }
+}
+
+// test cases
+
+// "Testing versions without subversion"
+console.log(compareVersions("11", "10")); // true
+
+// "Test equal versions"
+console.log(compareVersions("11", "11")); // true
+
+// "Adding a subversion should make this version \"larger\""
+console.log(compareVersions("10.4.6", "10.4")); // true
+
+// "Subversion precedence is smaller than Version"
+console.log(compareVersions("10.4", "11")); // false
+
+// "Version value is not the same as its decimal value"
+console.log(compareVersions("10.4", "10.10")); // false
+
+// "Adding subversion does not make it larger than a greater version"
+console.log(compareVersions("10.4.9", "10.5")); // false
+
+/* ------------------- Backspaces in String ------------------- */
+function cleanString(s) {
+  if (s.length === 0) return "";
+
+  let result = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "#") {
+      result.pop();
+    } else {
+      result.push(s[i]);
+    }
+  }
+
+  return result.join("");
+}
